686c65b991b8bb8625411bd27f040c5ffd8fd0b1
wicsay <124921503+ralina16@users.noreply.github.com>
2025-04-10 12:31:30 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,19 +67,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
-
-// Слайдер
-document.addEventListener("DOMContentLoaded", function () {
-    const slides = document.querySelectorAll(".slide");
-    let currentSlide = 0;
-
-    function showNextSlide() {
-        slides[currentSlide].classList.remove("active");
-
-        currentSlide = (currentSlide + 1) % slides.length;
-
-        slides[currentSlide].classList.add("active");
-    }
-
-    setInterval(showNextSlide, 3000);
-});
